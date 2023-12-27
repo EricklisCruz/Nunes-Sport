975761d78985fd98ef8ce5280ccdfbc8a679a607
EricklisCruz <erickliscruz@gmail.com>
2023-12-27 12:11:12 -0300

    [NS] - Creating project

---
+++ src/main/java/com/nunes/sport/Nunes/Sport/NunesSportApplication.java
@@ -0,0 +1,13 @@
+package com.nunes.sport.Nunes.Sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunesSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunesSportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nunes/sport/Nunes/Sport/NunesSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunes.sport.Nunes.Sport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunesSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
